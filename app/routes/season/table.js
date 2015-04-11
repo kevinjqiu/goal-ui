d3a3c35218cb9443c49b7a8f78a8de06d06ec333
Kevin Qiu <kevin@freshbooks.com>
2015-04-11 01:25:06 -0400

    Fix team link

--- app/routes/season/table.js
+++ app/routes/season/table.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     var season = this.modelFor('season');
+    this.controllerFor('season.table').set('seasonId', season.id);
     return this.store.find('table', {season_id: season.id});
   }
 });
